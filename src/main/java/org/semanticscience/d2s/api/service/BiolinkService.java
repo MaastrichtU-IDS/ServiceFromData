b34caa0be115f21bece2628932f751b3568de5d3
Vincent Emonet <vincent.emonet@gmail.com>
2020-03-03 22:32:33 +0100

    fix all biolink get calls

--- src/main/java/org/semanticscience/d2s/api/service/BiolinkService.java
+++ src/main/java/org/semanticscience/d2s/api/service/BiolinkService.java
@@ -14,10 +14,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 
 @RestController
@@ -27,91 +26,77 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 public class BiolinkService {
 	@SuppressWarnings("unused")
 	private static final Logger logger = Logger.getLogger(BiolinkService.class.getName());
-    
-    @Autowired
-	private RdfRepository repository;
-	
-    // http://docs.swagger.io/swagger-core/v2.0.0-RC3/apidocs/io/swagger/v3/oas/annotations/Operation.html
-	@RequestMapping(value = "/prefixes"
-    	, method = RequestMethod.GET
-		, produces = {ResultAs.CONTENT_TYPE_XML, ResultAs.CONTENT_TYPE_JSON, ResultAs.CONTENT_TYPE_CSV, ResultAs.CONTENT_TYPE_TSV})
-    @Operation(summary = "Returns all prefixes and their namespace URI used by the API.")
-    public void prefixes(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    	repository.handleApiCall(BiolinkQueryBuilder.datasets(), request, response);
-    }
 
-    @RequestMapping(value = "/datasets"
-    	, method = RequestMethod.GET
-		//, consumes = {ResultAs.CONTENT_TYPE_XML, ResultAs.CONTENT_TYPE_JSON, ResultAs.CONTENT_TYPE_CSV, ResultAs.CONTENT_TYPE_TSV}
-		//, produces = {ResultAs.CONTENT_TYPE_XML, ResultAs.CONTENT_TYPE_JSON, ResultAs.CONTENT_TYPE_CSV, ResultAs.CONTENT_TYPE_TSV}
-    	)
-    @Operation(summary="This call returns all datasets, which can be used as input for other services. Note that the first line in csv is the header.",
-		responses = {
-	      @ApiResponse(description = "Successful Operation", 
-    		  responseCode = "200", 
-				  content = { 
-						  @Content(mediaType = ResultAs.CONTENT_TYPE_TSV),
-						  @Content(mediaType = ResultAs.CONTENT_TYPE_CSV),
-						  @Content(mediaType = ResultAs.CONTENT_TYPE_JSON),
-						  @Content(mediaType = ResultAs.CONTENT_TYPE_XML)
-		         	  }
-	      )
-		}
-	  )
-    public void datasets(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    	repository.handleApiCall(BiolinkQueryBuilder.datasets(), request, response);
-    }
-    
-    @RequestMapping(value = "/get/{dataset}"
-	    , method = RequestMethod.GET
-		//, consumes = {ResultAs.CONTENT_TYPE_XML, ResultAs.CONTENT_TYPE_JSON, ResultAs.CONTENT_TYPE_CSV, ResultAs.CONTENT_TYPE_TSV}
-	    //, produces = {ResultAs.CONTENT_TYPE_XML, ResultAs.CONTENT_TYPE_JSON, ResultAs.CONTENT_TYPE_CSV, ResultAs.CONTENT_TYPE_TSV}
-    )
-    @Operation(summary="This all classes for this particular data-set with instances having an id.",
-		responses = {
-	      @ApiResponse(description = "Successful Operation", 
-    		  responseCode = "200", 
-    		  content = { 
-				  @Content(mediaType = ResultAs.CONTENT_TYPE_TSV),
-				  @Content(mediaType = ResultAs.CONTENT_TYPE_CSV),
-				  @Content(mediaType = ResultAs.CONTENT_TYPE_JSON),
-				  @Content(mediaType = ResultAs.CONTENT_TYPE_XML)
-         	  }
-		  	  //, schema = @Schema(implementation = String.class)
-	      )
-		}
-    )
-    public void classes(HttpServletRequest request, HttpServletResponse response
-    		, @PathVariable String dataset
-    		) throws IOException {
-    	repository.handleApiCall(BiolinkQueryBuilder.classes(dataset), request, response);
-    }
-    
-    @RequestMapping(value = "/get/{dataset}/{class}"
-    	, method = RequestMethod.GET
-		, consumes = {ResultAs.CONTENT_TYPE_XML, ResultAs.CONTENT_TYPE_JSON, ResultAs.CONTENT_TYPE_CSV, ResultAs.CONTENT_TYPE_TSV}
-    	, produces = {ResultAs.CONTENT_TYPE_XML, ResultAs.CONTENT_TYPE_JSON, ResultAs.CONTENT_TYPE_CSV, ResultAs.CONTENT_TYPE_TSV})
-    @Operation(summary="Returns all instances of a class. Default and maximum limit is 1000 instances per page. Use page parameter to load more.")
-    public void datasetClass(HttpServletRequest request, HttpServletResponse response
-    		, @Parameter(name = "Id of the dataset to query.") @PathVariable String dataset
-    		, @Parameter(name = "Class to retrieve informations about.") @PathVariable("class") String className
-    		, @RequestParam(required=false) Long page
-    		, @RequestParam(required=false) Long limit
-     		) throws IOException {
-    	repository.handleApiCall(BiolinkQueryBuilder.datasetClass(dataset, className, page, limit), request, response);
-    }
-	
-	@RequestMapping(value = "/get/{dataset}/{class}/{id}"
-    	, method = RequestMethod.GET
-		, consumes = {ResultAs.CONTENT_TYPE_XML, ResultAs.CONTENT_TYPE_JSON, ResultAs.CONTENT_TYPE_CSV, ResultAs.CONTENT_TYPE_TSV}
-    	, produces = {ResultAs.CONTENT_TYPE_XML, ResultAs.CONTENT_TYPE_JSON, ResultAs.CONTENT_TYPE_CSV, ResultAs.CONTENT_TYPE_TSV})
-    @Operation(summary="Loads all properties of a specific instance.")
-    public void datasetClassId(HttpServletRequest request, HttpServletResponse response
-    		, @Parameter(name = "Id of the dataset to query.") @PathVariable String dataset
-    		, @Parameter(name = "Class of the concept to retrieve.") @PathVariable("class") String className
-    		, @Parameter(name = "Id of the concept to retrieve.") @PathVariable String id
-    		) throws IOException {
-    	repository.handleApiCall(BiolinkQueryBuilder.datasetClassId(dataset, className, id), request, response);
+	@Autowired
+	private RdfRepository repository;
+
+	// http://docs.swagger.io/swagger-core/v2.0.0-RC3/apidocs/io/swagger/v3/oas/annotations/Operation.html
+	@RequestMapping(value = "/prefixes", 
+			method = RequestMethod.GET, 
+			produces = { ResultAs.CONTENT_TYPE_XML,ResultAs.CONTENT_TYPE_JSON, ResultAs.CONTENT_TYPE_CSV, ResultAs.CONTENT_TYPE_TSV }
+	)
+	@Operation(summary = "Returns all prefixes and their namespace URI used by the API.")
+	public void prefixes(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		repository.handleApiCall(BiolinkQueryBuilder.datasets(), request, response);
 	}
-    
+
+	@RequestMapping(value = "/datasets", 
+			method = RequestMethod.GET
+			//, produces = {ResultAs.CONTENT_TYPE_XML, ResultAs.CONTENT_TYPE_JSON, ResultAs.CONTENT_TYPE_CSV, ResultAs.CONTENT_TYPE_TSV}
+	)
+	@Operation(summary = "This call returns all datasets, which can be used as input for other services. Note that the first line in csv is the header.", 
+		responses = {
+			@ApiResponse(description = "Successful Operation", responseCode = "200", content = {
+					@Content(mediaType = ResultAs.CONTENT_TYPE_TSV), @Content(mediaType = ResultAs.CONTENT_TYPE_CSV),
+					@Content(mediaType = ResultAs.CONTENT_TYPE_JSON),
+					@Content(mediaType = ResultAs.CONTENT_TYPE_XML) }) })
+	public void datasets(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		repository.handleApiCall(BiolinkQueryBuilder.datasets(), request, response);
+	}
+
+	@RequestMapping(value = "/get/{dataset}", 
+			method = RequestMethod.GET)
+	@Operation(summary = "This all classes for this particular data-set with instances having an id.", 
+		responses = {
+			@ApiResponse(description = "Successful Operation", responseCode = "200", content = {
+					@Content(mediaType = ResultAs.CONTENT_TYPE_TSV), @Content(mediaType = ResultAs.CONTENT_TYPE_CSV),
+					@Content(mediaType = ResultAs.CONTENT_TYPE_JSON),
+					@Content(mediaType = ResultAs.CONTENT_TYPE_XML) }) })
+	public void classes(HttpServletRequest request, HttpServletResponse response, @PathVariable String dataset)
+			throws IOException {
+		repository.handleApiCall(BiolinkQueryBuilder.classes(dataset), request, response);
+	}
+
+	@RequestMapping(value = "/get/{dataset}/{class}", 
+			method = RequestMethod.GET )
+	@Operation(summary = "Returns all instances of a class. Default and maximum limit is 1000 instances per page. Use page parameter to load more.", 
+		responses = {
+			@ApiResponse(description = "Successful Operation", responseCode = "200", content = {
+				@Content(mediaType = ResultAs.CONTENT_TYPE_TSV), @Content(mediaType = ResultAs.CONTENT_TYPE_CSV),
+				@Content(mediaType = ResultAs.CONTENT_TYPE_JSON),
+				@Content(mediaType = ResultAs.CONTENT_TYPE_XML)}) })
+	public void datasetClass(HttpServletRequest request, HttpServletResponse response,
+			@PathVariable String dataset,
+			@PathVariable("class") String className,
+			@RequestParam(required = false) Long page, @RequestParam(required = false) Long limit) throws IOException {
+		repository.handleApiCall(BiolinkQueryBuilder.datasetClass(dataset, className, page, limit), request, response);
+	}
+
+	@RequestMapping(value = "/get/{dataset}/{class}/{id}", method = RequestMethod.GET, consumes = {
+			ResultAs.CONTENT_TYPE_XML, ResultAs.CONTENT_TYPE_JSON, ResultAs.CONTENT_TYPE_CSV,
+			ResultAs.CONTENT_TYPE_TSV }, produces = { ResultAs.CONTENT_TYPE_XML, ResultAs.CONTENT_TYPE_JSON,
+					ResultAs.CONTENT_TYPE_CSV, ResultAs.CONTENT_TYPE_TSV })
+	@Operation(summary = "Loads all properties of a specific instance.", 
+			responses = {
+					@ApiResponse(description = "Successful Operation", responseCode = "200", content = {
+						@Content(mediaType = ResultAs.CONTENT_TYPE_TSV), @Content(mediaType = ResultAs.CONTENT_TYPE_CSV),
+						@Content(mediaType = ResultAs.CONTENT_TYPE_JSON),
+						@Content(mediaType = ResultAs.CONTENT_TYPE_XML)}) })
+	public void datasetClassId(HttpServletRequest request, HttpServletResponse response,
+			@PathVariable String dataset,
+			@PathVariable("class") String className,
+			@PathVariable String id) throws IOException {
+		repository.handleApiCall(BiolinkQueryBuilder.datasetClassId(dataset, className, id), request, response);
+	}
+
 }
