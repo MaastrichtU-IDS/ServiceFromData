9dbeaa586d135ac3f75cbadffaf9430b24adc31b
Vincent Emonet <vincent.emonet@gmail.com>
2020-03-04 08:10:00 +0100

    improve Remote KG docs

--- src/main/java/org/semanticscience/d2s/api/model/Message.java
+++ src/main/java/org/semanticscience/d2s/api/model/Message.java
@@ -27,14 +27,16 @@ public class Message {
 	// TODO: this should be able to be either a KnowledgeGraph OR a RemoteKnowledgeGraph
 	@Schema(description = "KnowledgeGraph object that contains all the nodes and edges referenced" + 
 			" in any of the possible answers to the query OR connection information" + 
-			" for a remote knowledge graph")
+			" for a remote knowledge graph",
+			required = false)
 	private KnowledgeGraph knowledge_graph;
 	public KnowledgeGraph getKnowledge_graph() {
 		return knowledge_graph;
 	}
 	
-	@Schema(description = "KnowledgeGraph object that contains  connection information" + 
-			" for a remote knowledge graph")
+	@Schema(description = "RemoteKnowledgeGraph object that contains  connection information" + 
+			" for a remote knowledge graph connection (not used if knowledge_graph defined)",
+			required = false)
 	private RemoteKnowledgeGraph remote_knowledge_graph;
 	public RemoteKnowledgeGraph getRemote_knowledge_graph() {
 		return remote_knowledge_graph;
