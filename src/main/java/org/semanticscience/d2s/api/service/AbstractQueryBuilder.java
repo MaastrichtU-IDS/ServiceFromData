63527d824825e5f4b6dcad5f60f1637886773f40
Vincent Emonet <vincent.emonet@gmail.com>
2020-03-08 00:38:17 +0100

    add omop prefix

--- src/main/java/org/semanticscience/d2s/api/service/AbstractQueryBuilder.java
+++ src/main/java/org/semanticscience/d2s/api/service/AbstractQueryBuilder.java
@@ -24,7 +24,7 @@ public class AbstractQueryBuilder {
 		+ "PREFIX ncit: <http://ncicb.nci.nih.gov/xml/owl/EVS/Thesaurus.owl#>\n"
 		+ "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>\n"
 		+ "PREFIX schema: <http://schema.org/>\n"
-		+ "PREFIX omopcohd: <http://api.ohdsi.org/WebAPI/vocabulary/concept/>\n";
+		+ "PREFIX omop: <http://api.ohdsi.org/WebAPI/vocabulary/concept/>\n";
 	
 	protected static String paginate(Long page, Long limit) {
 		if (limit == null || limit > LIMIT || limit<1L)
