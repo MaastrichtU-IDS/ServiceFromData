449bdd76064841df863f8839561b70b07a0461eb
Vincent Emonet <vincent.emonet@gmail.com>
2020-05-06 23:31:49 +0200

    remove unused tags import

--- src/main/java/org/semanticscience/d2s/api/Application.java
+++ src/main/java/org/semanticscience/d2s/api/Application.java
@@ -14,7 +14,6 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
-import io.swagger.v3.oas.annotations.tags.Tag;
 
 @OpenAPIDefinition(
 	info = @Info(title = "TReK Clinical Reasoner API", 
