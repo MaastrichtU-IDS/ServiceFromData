ce96e2ae8900267201a6638e4fac56067e961309
Vincent Emonet <vincent.emonet@gmail.com>
2020-05-06 18:36:23 +0200

    update title of Open API

--- src/main/java/org/semanticscience/d2s/api/Application.java
+++ src/main/java/org/semanticscience/d2s/api/Application.java
@@ -16,12 +16,13 @@ import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
 
 @OpenAPIDefinition(
-	info = @Info(title = "Translator Red Knowledge API", 
-		description = "This endpoint implements the [Reasoner API specifications](https://github.com/NCATS-Tangerine/NCATS-ReasonerStdAPI/tree/master/API) to query clinical data from the Columbia Open Health Data (COHD) integrated in a [BioLink-complying RDF](https://biolink.github.io/biolink-model/) triplestore, through a SPARQL endpoint. \n \n "
+	info = @Info(title = "TReK Reasoner API", 
+		description = "Reasoner API for the Translator REd Knowledge graph, a BioLink-compliant RDF knowledge graph about Clinical Data.\n\n"
+				+ "This endpoint implements the [Reasoner API specifications](https://github.com/NCATS-Tangerine/NCATS-ReasonerStdAPI/tree/master/API) to query clinical data from the Columbia Open Health Data (COHD) integrated in a [BioLink-complying RDF](https://biolink.github.io/biolink-model/) triplestore, through a SPARQL endpoint. \n \n "
 				+ "COHD was developed at the [Columbia University Department of Biomedical Informatics](https://www.dbmi.columbia.edu/) "
 				+ "as a collaboration between the [Weng Lab](http://people.dbmi.columbia.edu/~chw7007/), [Tatonetti Lab](http://tatonettilab.org/), and the [NCATS Biomedical Data Translator program](https://ncats.nih.gov/translator) (Red Team).\n\n"
 				+ "This work was supported in part by grants: NCATS OT3TR002027, NLM R01LM009886-08A1, and NIGMS R01GM107145.\n\n"
-				+ "This API is registered as a Smart API at [https://smart-api.info/registry?q=bc58469a9ec0567d4ca0032b35d27b7d](https://smart-api.info/registry?q=bc58469a9ec0567d4ca0032b35d27b7d)\n\n" +
+				+ "This API is registered as a Smart API at [http://trek.smart-api.info/](http://trek.smart-api.info/)\n\n" +
 				"The following external resources may be useful:\n" + 
 				"* [Reasoner API Specifications](https://github.com/NCATS-Tangerine/NCATS-ReasonerStdAPI/tree/master/API)\n" +
 				"* [BioLink model documentation](https://biolink.github.io/biolink-model/)\n" +
