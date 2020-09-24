d9efadd5e42410ca828d38ba1ae6e30e3154cbf5
Vincent Emonet <vincent.emonet@gmail.com>
2020-09-24 17:58:19 +0200

    update descriptions

--- src/main/java/org/semanticscience/d2s/api/Application.java
+++ src/main/java/org/semanticscience/d2s/api/Application.java
@@ -16,27 +16,19 @@ import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
 
 @OpenAPIDefinition(
-	info = @Info(title = "TReK Clinical Reasoner API", 
-		description = "Reasoner API for the **T**ranslator **Re**d **K**nowledge graph, a BioLink-compliant RDF knowledge graph about Clinical Data.\n\n"
-				+ "This endpoint implements the [Reasoner API specifications](https://github.com/NCATS-Tangerine/NCATS-ReasonerStdAPI/tree/master/API) to query clinical data from the Columbia Open Health Data (COHD) integrated in a [BioLink-complying RDF](https://biolink.github.io/biolink-model/) triplestore, through a SPARQL endpoint. \n \n "
-				+ "COHD was developed at the [Columbia University Department of Biomedical Informatics](https://www.dbmi.columbia.edu/) "
-				+ "as a collaboration between the [Weng Lab](http://people.dbmi.columbia.edu/~chw7007/), [Tatonetti Lab](http://tatonettilab.org/), and the [NCATS Biomedical Data Translator program](https://ncats.nih.gov/translator) (Red Team).\n\n"
+	info = @Info(title = "Knowledge Collaboratory Reasoner API", 
+		description = "Reasoner API for the Translator Personal Scientific Knowledge Graph and Knowledge Collaboratory.\n\n"
+				+ "This endpoint implements the [Reasoner API specifications](https://github.com/NCATS-Tangerine/NCATS-ReasonerStdAPI/tree/master/API) to query Nanopublications integrated in a [BioLink-complying RDF](https://biolink.github.io/biolink-model/) triplestore, through a SPARQL endpoint. \n \n "
 				+ "This work was supported in part by grants: NCATS OT3TR002027, NLM R01LM009886-08A1, and NIGMS R01GM107145.\n\n"
-				+ "This API is registered as a Smart API at [trek.smart-api.info](http://trek.smart-api.info)\n\n" +
 				"The following external resources may be useful:\n" + 
 				"* [Reasoner API Specifications](https://github.com/NCATS-Tangerine/NCATS-ReasonerStdAPI/tree/master/API)\n" +
-				"* [BioLink model documentation](https://biolink.github.io/biolink-model/)\n" +
-				"* [Resolve COHD OMOP identifiers](http://api.ohdsi.org/WebAPI/vocabulary/concept/922802)\n" +
-				"* [OHDSI](https://www.ohdsi.org/)\n" + 
-				"* [OMOP Common Data Model](https://github.com/OHDSI/CommonDataModel/wiki)\n" + 
-				"* [Athena](http://athena.ohdsi.org/) (OMOP vocabularies, search, concept relationships, concept hierarchy)\n" + 
-				"* [Atlas](http://www.ohdsi.org/web/atlas/) (OMOP vocabularies, search, concept relationships, concept hierarchy, concept sets)",
+				"* [BioLink model documentation](https://biolink.github.io/biolink-model/)\n" +,
 		termsOfService = "https://opensource.org/licenses/MIT",
 		version = "1.0",
 		license = @License(name = "MIT License", url = "https://opensource.org/licenses/MIT"),
 		contact = @Contact(name = "Institute of Data Science at Maastricht University",
 				email = "vincent.emonet@maastrichtuniversity.nl",
-				url = "https://d2s.semanticscience.org"))
+				url = "https://github.com/vemonet/"))
 )
 @SpringBootApplication
 @RestController
